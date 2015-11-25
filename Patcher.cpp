865502643ff5c0f20cec29bcdf6d25b94bcceaf0
X-Boris <borismuzellec@free.fr>
2015-11-25 10:19:58 +0100

    implemented patcher builder

--- Patcher.cpp
+++ Patcher.cpp
@@ -3,3 +3,9 @@
 //
 
 #include "Patcher.h"
+
+Patcher::Patcher(const Mat& Patch, size_t width, size_t height) : patch(patch), output(Mat::zeros(height, width, CV_8UC3)), oldSeams(Mat::zeros(height, width,CV_32F)){};
+
+const Mat& Patcher::randomStep(){
+
+};
